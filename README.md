5b4f0cdcdeae389d3c2e8921fe6d7d625dc1784f
Kevin Piña <kevin@outlook.com>
2025-06-05 13:25:04 +0200

    Simple JAX WS

---
+++ README.md
@@ -0,0 +1,3 @@
+## JAX-WS - "Java API for Web Service, SOAP" (jakarta-ee9-JAX-WS)
+* feature/01_simple-jax-ws (jax-ws)
+
---
+++ jax-ws/jakarta-ee9-jaxws-client/src/main/java/com/kevinpina/Main.java
@@ -0,0 +1,29 @@
+package com.kevinpina;
+
+import com.kevinpina.services.Course;
+import com.kevinpina.services.CourseServiceWS;
+import com.kevinpina.services.CourseServiceWSImplService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Setting these Properties to print the XML request used to call the WebService.
+        System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+        System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dumpTreshold", "999999");
+
+        CourseServiceWS courseServiceWS = new CourseServiceWSImplService().getCourseServiceWSImplPort();
+        System.out.println(courseServiceWS.inform("Hola Mundo"));
+
+        Course course = new Course();
+        course.setName("Microsoft");
+        Course courCreated = courseServiceWS.create(course);
+        System.out.println(courCreated.getName());
+
+        courseServiceWS.list().forEach(c -> System.out.println(c.getName()));
+    }
+
+}
